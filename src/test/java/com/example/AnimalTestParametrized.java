f0f8c4ccd231aff47aa79c53b0635fa5566be5a0
Vladimir Khalturin <voilik14@yandex.ru>
2023-02-05 18:32:19 +0700

    Sprint_6_first_commit

--- src/test/java/com/example/AnimalTestParametrized.java
+++ src/test/java/com/example/AnimalTestParametrized.java
@@ -1,2 +1,40 @@
-package com.example;public class AnimalTestParametrized {
+package com.example;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+
+@RunWith(Parameterized.class)
+public class AnimalTestParametrized {
+
+    private String animalKind;
+    private List<String> listOfFood;
+
+    public AnimalTestParametrized(String animalKind, List<String> listOfFood) {
+        this.animalKind = animalKind;
+        this.listOfFood = listOfFood;
+    }
+    @Before
+    public  void initMockito() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Parameterized.Parameters
+    public  static Object [][] setAnimalKind () {
+        return new Object[][] {
+                {"Травоядное",List.of("Трава", "Различные растения")},
+                {"Хищник", List.of("Животные", "Птицы", "Рыба")},
+        };
+    }
+
+    @Test
+    public  void animalGetFood() throws Exception {
+        Animal animal = new Animal();
+        Assert.assertEquals("Неизвестный вид животного, используйте значение Травоядное или Хищник", listOfFood, animal.getFood(animalKind));
+    }
 }
--- src/test/java/com/example/LionTestParametrized.java
+++ src/test/java/com/example/LionTestParametrized.java
@@ -1,2 +1,51 @@
-package com.example;public class LionTestParametrized {
+package com.example;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.MockitoAnnotations;
+
+@RunWith(Parameterized.class)
+public class LionTestParametrized {
+    private  String sex;
+    private boolean hasMane;
+
+    private int kittensCount;
+
+    public LionTestParametrized(String sex, boolean hasMane, int kittensCount) {
+        this.sex = sex;
+        this.hasMane = hasMane;
+        this.kittensCount = kittensCount;
+    }
+
+    @Before
+    public  void initMockito() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Parameterized.Parameters
+    public  static Object [][] setLionSex () {
+        return new Object[][] {
+                {"Самец", true, 1},
+                {"Самка", false, 1}
+        };
+    }
+
+    @Test
+    public  void lionDoesHaveMane() throws Exception {
+        Feline feline = new Feline();
+        Lion lion = new Lion(feline, sex);
+
+        Assert.assertEquals("Используйте допустимые значения пола животного - самец или самка", hasMane, lion.doesHaveMane());
+    }
+
+    @Test
+    public void lionGetKittens() throws  Exception{
+        Feline feline = new Feline();
+        Lion lion = new Lion(feline, sex);
+
+        Assert.assertEquals("У львов 1 львенок", kittensCount, lion.getKittens());
+    }
 }
